afa760d82148d826910c35b06fe8bcb187104de7
Davi Bertotto <dbertotto@inf.ufpel.edu.br>
2026-09-09 13:16:33 -0300

    Atualize Structs

---
+++ Structs/programa4.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+
+//enum --> Enumeração
+
+enum dias_da_semana{
+    segunda,
+    terca,
+    quarta,
+    quinta,
+    sexta,
+    sabado,
+    domingo
+};
+
+int main(){
+
+
+
+    return 0;
+}
+
+
